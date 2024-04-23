a436876d0b060dcc6d00c31cf8a5ce9f9ec7eff4
Ariya C <acoleman@bsd220.org>
2024-04-23 10:04:53 -0500

    Fixed errors

--- 'Za Hunter/ContentView.swift
+++ 'Za Hunter/ContentView.swift
@@ -10,6 +10,7 @@ import MapKit
 
 struct ContentView: View {
     @StateObject var locationManager = LocationManager()
+    @State private var places = [Place]()
     @State private var userTrackingMode: MapUserTrackingMode = .follow
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(
@@ -24,8 +25,32 @@ struct ContentView: View {
                 coordinateRegion: $region,
                 interactionModes: .all,
                 showsUserLocation: true,
-                userTrackingMode: $userTrackingMode
-            )
+                userTrackingMode: $userTrackingMode,
+                annotationItems: places) { place in
+                    MapAnnotation(coordinate: place.annotation.coordinate) {
+                        Marker(mapItem: place.mapItem)
+                    }
+                }
+                .onAppear(perform: {
+                    performSearch(item: "Pizza")
+                })
+        }
+    }
+    
+    func performSearch(item: String) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = region
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { response, error in
+            if let response = response {
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    places.append(Place(annotation: annotation, mapItem: mapItem))
+                }
+            }
         }
     }
 }
@@ -33,5 +58,22 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Place: Identifiable {
+    let id = UUID()
+    let annotation: MKPointAnnotation
+    let mapItem: MKMapItem
+}
+
+struct Marker: View {
+    var mapItem: MKMapItem
+    var body: some View {
+        if let url = mapItem.url {
+            Link(destination: url, label: {
+                Image("pizza")
+            })
+        }
     }
 }
